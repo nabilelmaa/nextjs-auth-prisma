27646155b0511db2ff35b677efdb17f4a30c5b74
Nabil El Maaroufi <93434382+nabilelmaa@users.noreply.github.com>
2024-04-26 12:25:23 +0100

    new change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import Image from "next/image";
 
 function Navbar() {
   return (
-    <div className="max-w-full p-2 flex items-center justify-end border rounded-md bg-blue-100">
+    <div className="max-w-full p-2 flex items-center justify-between border shadow-md rounded-md bg-gray-100">
+      <Link href="/">
+        <Image
+          src="/cover.png"
+          alt="logo"
+          width={35}
+          height={35}
+          className="ml-4"
+        />
+      </Link>
+
       <Link href="/sign-in">
-        <button className="p-2 w-24 border rounded-full bg-blue-600 text-white hover:bg-blue-700 transition duration-300">Sign In</button>
+        <button className="p-2 w-24 rounded-full bg-blue-600 text-white hover:bg-blue-700 transition duration-300">
+          Sign In
+        </button>
       </Link>
     </div>
   );
